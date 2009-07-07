3ece797f9de8cd514c0a26d61d1e99adf994246d
felix <felix8a@gmail.com>
2009-07-06 17:26:07 -0700

    document.documentElement.innerHTML has a weird value
    If you cajole this in the testbed
      <script>document.documentElement.innerHTML</script>
    you get back a weird value:
      "<head>function () {\n return this.innerHTMLGetter___();\n}</head><body></body>"
    
    This change fixes it.

--- src/com/google/caja/plugin/domita.js
+++ src/com/google/caja/plugin/domita.js
@@ -2961,7 +2961,8 @@ var attachDocumentStub = (function () {
           function () { return [tameHeadElement, tameBodyElement]; },
           function () { return tameDoc; },
           function () {
-            return ('<head>' + tameHeadElement.getInnerHTML + '<\/head><body>'
+            return ('<head>' + tameHeadElement.getInnerHTML()
+                    + '<\/head><body>'
                     + tameBodyElement.getInnerHTML() + '<\/body>');
           },
           tameBody,
